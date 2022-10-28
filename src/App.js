e9028cccec3af6b825e2f09ffab0d2c9bf18bff6
KalebLight <kaleb.luiz00@gmail.com>
2022-10-28 11:46:55 -0300

    Alterando arquivos para pasta raiz e adicionando containers de categorias

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import categories from './api';
+import Row from './components/Row/Row';
+function App() {
+  return (
+    <div className="App">
+      {categories.map((category) => {
+        return (
+          <Row
+            key={category.name}
+            title={category.title}
+            path={category.path}
+          />
+        );
+      })}
+    </div>
+  );
+}
+
+export default App;
