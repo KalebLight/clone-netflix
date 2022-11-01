8050ebc44cab2dd8ee46b9f4577291a5dfb3402e
KalebLight <kaleb.luiz00@gmail.com>
2022-11-01 11:11:52 -0300

    Corrigindo path dos generos dos filmes

--- src/api.js
+++ src/api.js
@@ -4,32 +4,32 @@ const categories = [
   {
     name: 'trending',
     title: 'Em alta',
-    path: `/trending/all/week?api_key=${API_KEY}&language=pt-BR`,
+    path: `trending/all/week?api_key=${API_KEY}&language=pt-BR`,
   },
   {
     name: 'netflixOriginals',
     title: 'Originais Netflix',
-    path: `/discover/tv?api_key=${API_KEY}&with_companies=213`,
+    path: `discover/tv?api_key=${API_KEY}&with_networks=213`,
   },
   {
     name: 'topRated',
     title: 'Populares',
-    path: `/movie/top_rated?api_key=${API_KEY}&language=pt-BR`,
+    path: `movie/top_rated?api_key=${API_KEY}&language=pt-BR`,
   },
   {
     name: 'romances',
     title: 'Romances',
-    path: `/discover/tv?api_key=${API_KEY}&with_genre=10749`,
+    path: `discover/movie?api_key=${API_KEY}&with_genres=10749`,
   },
   {
     name: 'comedy',
     title: 'Comédias',
-    path: `/discover/tv?api_key=${API_KEY}&with_genre=35`,
+    path: `discover/movie?api_key=${API_KEY}&with_genres=35`,
   },
   {
     name: 'documentaries',
     title: 'Documentários',
-    path: `/discover/tv?api_key=${API_KEY}&with_genre=99`,
+    path: `discover/movie?api_key=${API_KEY}&with_genres=99`,
   },
 ];
 
